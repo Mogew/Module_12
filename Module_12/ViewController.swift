f5735c1fae32375cf8c0478e727faf6c8eeeb9cb
Mogew <alimovren@gmail.com>
2023-04-24 06:33:15 +0600

    Initial Commit

---
+++ Module_12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Module_12
+//
+//  Created by Renat on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
